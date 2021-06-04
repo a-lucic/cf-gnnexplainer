5302b66b523dd4c7d736002d44e8337cc58f5d42
Ana Lucic <31237441+a-lucic@users.noreply.github.com>
2021-06-04 16:47:56 +0200

    Update gcn.py

--- src/gcn.py
+++ src/gcn.py
@@ -69,32 +69,3 @@ class GCNSynthetic(nn.Module):
 
     def loss(self, pred, label):
         return F.nll_loss(pred, label)
-
-
-
-
-
-class GCNSynthetic_v2(nn.Module):
-    """
-    3-layer GCN used in GNN Explainer synthetic tasks, including
-    """
-    def __init__(self, nfeat, nhid, nout, nclass, dropout):
-        super(GCNSynthetic_v2, self).__init__()
-
-        self.gc1 = GCNConv(nfeat, nhid)
-        self.gc2 = GCNConv(nhid, nhid)
-        self.gc3 = GCNConv(nhid, nout)
-        self.lin = nn.Linear(nhid + nhid + nout, nclass)
-        self.dropout = dropout
-
-    def forward(self, x, edge_index):
-        x1 = F.relu(self.gc1(x, edge_index))
-        x1 = F.dropout(x1, self.dropout, training=self.training)
-        x2 = F.relu(self.gc2(x1, edge_index))
-        x2 = F.dropout(x2, self.dropout, training=self.training)
-        x3 = self.gc3(x2, edge_index)
-        x = self.lin(torch.cat((x1, x2, x3), dim=1))
-        return F.log_softmax(x, dim=1)
-
-    def loss(self, pred, label):
-        return F.nll_loss(pred, label)
